a64a4ae31546ec2b78929ff1edd09ebe0f7805b7
zacqueropattiwael-dev <zacqueropattiwael@gmail.com>
2026-07-01 12:47:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,74 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 const btn = document.getElementById("boomBtn");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener("resize", () => {
+function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-});
+}
+resize();
+window.addEventListener("resize", resize);
 
 let particles = [];
 
-class Particle{
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
 
-    constructor(x,y){
+        this.radius = Math.random() * 3 + 1;
 
-        this.x=x;
-        this.y=y;
+        this.vx = (Math.random() - 0.5) * 12;
+        this.vy = (Math.random() - 0.5) * 12;
 
-        this.radius=Math.random()*3+1;
+        this.life = 120;
 
-        this.dx=(Math.random()-0.5)*12;
-        this.dy=(Math.random()-0.5)*12;
-
-        this.life=120;
-
-        this.color=`hsl(${Math.random()*360},100%,60%)`;
-
+        this.color = `hsl(${Math.random() * 360},100%,60%)`;
     }
 
-    update(){
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
 
-        this.x+=this.dx;
-        this.y+=this.dy;
-
-        this.dy+=0.05;
+        this.vy += 0.05; // gravitasi
 
         this.life--;
-
     }
 
-    draw(){
-
+    draw() {
         ctx.beginPath();
-
-        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2);
-
-        ctx.fillStyle=this.color;
-
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
         ctx.fill();
-
     }
-
 }
 
-function animate(){
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    for(let i=particles.length-1;i>=0;i--){
-
+    for (let i = particles.length - 1; i >= 0; i--) {
         particles[i].update();
-
         particles[i].draw();
 
-        if(particles[i].life<=0){
-
-            particles.splice(i,1);
-
+        if (particles[i].life <= 0) {
+            particles.splice(i, 1);
         }
-
     }
 
     requestAnimationFrame(animate);
-
 }
 
 animate();
 
-btn.onclick=()=>{
+btn.addEventListener("click", () => {
 
-    btn.style.display="none";
+    btn.style.display = "none";
 
     const x = window.innerWidth / 2;
-const y = window.innerHeight / 2;
+    const y = window.innerHeight / 2;
 
-    for(let i=0;i<150;i++){
-
-        particles.push(new Particle(x,y));
-
+    for (let i = 0; i < 150; i++) {
+        particles.push(new Particle(x, y));
     }
 
-}
+});
